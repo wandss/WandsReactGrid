78762842445d0648b471f1216be56403e41b70e7
wandss <wandss@gmail.com>
2018-05-10 00:37:03 -0300

    added TODOS

--- wandsgridreact/src/index.js
+++ wandsgridreact/src/index.js
@@ -11,19 +11,21 @@ class Index extends React.Component{
   }
 
   render(){
-    const movies = [{id:1, movie:"Monty Python and the Holy Grail", year:1975},
-                    {id:2, movie:"Pulp Fiction", year:1994},
-                    {id:3, movie:"The Green Mile", year:1999},]
-    const colNames = {'movie':'Filmes','year':'Ano'};
+    const movies = [{id:1, movie:"Monty Python and the Holy Grail", year:1975, genre:'Comedy'},
+                    {id:2, movie:"Pulp Fiction", year:1994, genre:['Drama', 'Crime']},
+                    {id:3, movie:"The Green Mile", year:1999,genre:['Crime','Drama','Fantasy','Mistery']},
+                    {id:4, movie:"Requiem For a Dream", year:1999, genre:'Drama'},
+    ]
+    const colNames = {'movie':'Filmes','year':'Ano','genre':'Gênero'};
     return(
       <div>
         <GridComponent searchField
          getRowId={this.rowId}
          getRow={this.row}
          trueValueProps={{cssClass:'fa fa-check', color:'green'}}
-         hiddenColumns={['sold_out']}
+         hiddenColumns={['year']}
          renameColumns={colNames}
-         removeColumns={['id', 'Ano']}
+         removeColumns={['id']}
          gridData={movies}
         />
       </div>
